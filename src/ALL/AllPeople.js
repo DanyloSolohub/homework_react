08b02e9b8249fbea94cdc08b8751e2bd4f660086
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2020-12-12 22:01:55 +0200

    posts btn onn off

--- src/ALL/AllPeople.js
+++ src/ALL/AllPeople.js
@@ -7,19 +7,22 @@ import PostsComp from "../Components/PostsComp";
 class AllPeople extends Component {
     UService = new UsersService()
     PService = new PostsService()
-    state = {users:[],posts:[],userPost:null,flag:false}
-        showPost = (id) => this.setState({userPost:this.state.posts.filter(value => value.userId === id)})
+    state = {users:[],posts:[],userPost:null,flag:false,flag2:false}
+        showPost = (id) => {
+        this.setState({userPost:this.state.posts.filter(value => value.userId === id)})
+        this.setState({flag2:!this.state.flag2})
+    }
     Backinfo = () => this.setState({flag:!this.state.flag})
 
    render() {
-        let {users,userPost,flag} = this.state
+        let {users,userPost,flag,flag2} = this.state
         return (
             <div>
                 {
                     users.map((value) => <PeopleComponent item = {value}  key = {value.id} showPost={this.showPost} Backinfo={this.Backinfo} flag={flag}/>)
                 }
                 {
-                  userPost && userPost.map(value => <PostsComp item = {value} key = {value.id}/>)
+                 flag2 && userPost && userPost.map(value => <PostsComp item = {value} key = {value.id}/>)
                 }
             </div>
         );
