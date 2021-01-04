dbd22684246ea6bdf708cca9b5a5e10dc61e2ace
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-01-04 15:07:16 +0200

    react redux counter + todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React, {Component} from 'react';
+import React,{useEffect} from 'react'
+import {useDispatch,useSelector} from "react-redux";
+import {incCounter,decCounter,resetCounter} from './redux/action-creators/index'
+import {setTodos} from './redux/action-creators/index'
+import './App.css'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+export default function App()  {
+
+    const {counter,todos} = useSelector(({counter: {counter},todos:{todos}})=> ({counter,todos}))
+    const dispatch = useDispatch()
+
+
+
+    const handleinc = () => dispatch(incCounter())
+    const handledec = () => dispatch(decCounter())
+    const handlereset = () => dispatch(resetCounter())
+
+
+
+    useEffect(()=>{
+    fetchtodos()
+    },[counter])
+
+    const fetchtodos = async ()=>{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+        const json = await response.json()
+       dispatch(setTodos(json))
+
     }
-}
+    console.log(todos)
+return(
+        <div className={'main_div'}>
+     <h1>Counter {counter}</h1>
+           <div> <button onClick={handleinc}>inc</button>
+            <button onClick={handledec}>dec</button>
+            <button onClick={handlereset}>reset</button>
+        </div>
+            {( !!todos && <h2 >
+                        {todos.id}. {todos.title}
+                    </h2>)
 
-export default App;
+            }
+        </div>
+      );
+}
---
+++ src/redux/reducers/counter-reducer.js
@@ -0,0 +1,22 @@
+import {INC,DEC,RESET} from '../action-types/index'
+
+const initialState = {
+    counter :0
+}
+
+ const reducer = (state=initialState,action) =>{
+    switch(action.type){
+        case INC :{
+            return {...state ,counter: state.counter+1}
+        }case DEC :{
+            return{...state, counter:state.counter-1}
+        }case RESET :{
+            return{...state, counter:1}
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+export default reducer;
